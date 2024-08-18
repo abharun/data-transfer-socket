852bd2882c19185b5671b6891b97d66f8bb0b5fd
ABH <155776685+abharun@users.noreply.github.com>
2024-08-18 17:36:26 +0200

    feat: websocket server impl

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,36 @@
-import { MESSAGES } from "consts"
+import { MESSAGES } from "consts";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import { Logger } from "utils";
+import { WebSocketServer, WebSocket } from "ws";
+import { v4 as uuidv4 } from "uuid";
 
 dotenv.config();
+
+interface Message {
+  id: string;
+  msg: string;
+}
+
+const wss = new WebSocketServer({ port: 8000 });
+
+wss.on("connection", (ws: WebSocket) => {
+  const clientID = uuidv4();
+  Logger.info("New Client connected: ", clientID);
+
+  ws.send(JSON.stringify({ type: "id", id: clientID }));
+
+  ws.on("message", (message: string) => {
+    const msg: Message = JSON.parse(message);
+    Logger.info("Received from client ", msg.id);
+    Logger.info("Message: ", msg.msg);
+  });
+
+  ws.on("close", () => {
+    Logger.info(`Client ${clientID} disconnected`);
+  });
+});
 
 const app = express();
 
@@ -16,7 +42,7 @@ app
 const PORT = process.env.SERBER_PORT || 4000;
 
 app.listen(PORT, () => {
-    Logger.log(MESSAGES.MSG_SERVER_STARTED);
-  });
+  Logger.log(MESSAGES.MSG_SERVER_STARTED);
+});
 
 export default app;
