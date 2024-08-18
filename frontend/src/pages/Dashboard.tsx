f961b484299f458de5603250074ee66a2c053777
ABH <155776685+abharun@users.noreply.github.com>
2024-08-18 18:00:28 +0200

    feat: data input working

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { withMainlayout } from "../layouts";
 
 export const Dashboard: React.FC = withMainlayout(() => {
+  const [id, setId] = useState<string | null>(null);
+  let username: string = "";
+  let strdata: string = "";
+
+  const sendData = () => {
+    console.log(`Username: ${username}, Data: ${strdata}`);
+  };
+
   return (
     <div className="flex w-full h-full justify-center items-center">
       <div className="flex-col w-1/2 h-2/3 p-2 border-2 border-black rounded-lg">
@@ -14,17 +22,32 @@ export const Dashboard: React.FC = withMainlayout(() => {
               <span>Username</span>
             </div>
             <div className="input-user flex p-2">
-              <input type="text" className="w-full border" />
+              <input
+                type="text"
+                className="w-full border"
+                onChange={(e) => {
+                  username = e.target.value;
+                }}
+              />
             </div>
             <div className="label-string flex p-2">
               <span>Data</span>
             </div>
             <div className="input-string flex p-2">
-              <input type="text" className="w-full border" />
+              <input
+                type="text"
+                className="w-full border"
+                onChange={(e) => {
+                  strdata = e.target.value;
+                }}
+              />
             </div>
             <div className="btn-send flex p-2 justify-center">
-              <button className="border-2 rounded-xl p-2 border-black">
-                Send
+              <button
+                className="border-2 rounded-xl p-2 border-black"
+                onClick={sendData}
+              >
+                <span>Send</span>
               </button>
             </div>
           </div>
